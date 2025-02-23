37e7f7b3a6f678f39bf50464f79cfcccd3c9b087
Yash Vashishth <imyashvashishth05@gmail.com>
2025-02-23 20:55:54 +0530

    y

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
+const DB = process.env.MONGODB_URI
 const connectDB =async()=>{
 
     mongoose.connection.on('connected',()=>{
-        console.log("Database connected")
+        console.log("Database connected");
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}`)
+    await mongoose.connect(DB)
 }
 
-export default connectDB
+export default connectDB;
